f33a86b53df5b8130d03fe27831dc70ffedbaae8
Louvivien <louvivien@hotmail.com>
2024-02-11 16:09:33 +0100

    Add manager role

--- src/pages/CustomerOrdersPage.jsx
+++ src/pages/CustomerOrdersPage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Container, Heading, Text } from '@chakra-ui/react';
 import { Layout } from '../components/Layout';
 import VacationRequestsList from '../components/VacationRequestsList';
-import VacationRequestsListAdmin from '../components/VacationRequestsListAdmin'; // Import the admin version
+import VacationRequestsListAdmin from '../components/VacationRequestsListAdmin'; 
+import VacationRequestsListManager from '../components/VacationRequestsListManager';
 import { useAuth } from '../contexts/AuthContext'; // Import useAuth to access the user's role
 
 const CustomerOrdersPage = () => {
@@ -12,11 +13,19 @@ const CustomerOrdersPage = () => {
     <Layout>
       <Container maxW='container.lg' py={4}>
         <Heading as="h1">Vos demandes</Heading>
-        <Text mb={4}>Liste de toutes les demandes que vous avez faites.</Text>
         {
-          // Conditionally render based on the user's role
-          userRole === 'admin' ? <VacationRequestsListAdmin /> : <VacationRequestsList />
-        }
+            // Conditionally render based on the user's role
+            userRole === 'admin' || userRole === 'manager' ? 
+            <Text mb={4}>Liste de toutes les demandes</Text> : 
+            <Text mb={4}>Liste de toutes les demandes que vous avez faites</Text>
+          }
+
+        {
+            // Conditionally render based on the user's role
+            userRole === 'admin' ? <VacationRequestsListAdmin /> : 
+            userRole === 'manager' ? <VacationRequestsListManager /> : 
+            <VacationRequestsList />
+          }
       </Container>
     </Layout>
   );
